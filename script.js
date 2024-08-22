27a0770a4f1a47a9a80dd2dfed17ecc3bb2b4b03
Marwan Samaha <marwansamaha13@gmail.com>
2024-08-22 03:09:39 +0300

    Upload

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
     var typed = new Typed(".typing", {
         strings: ["Brand Manager", "Product Manager"],
-        typeSpeed: 110,
+        typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 });
+
+let main = document.querySelector(".main")
+let toggler = document.querySelector("nav")
+
+toggler.addEventListener('clicked', function(){
+    main.remove
+    console.log("hi")
+})
